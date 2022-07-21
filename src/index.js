480c7824db814e22330aac5fbf1afb43e6deae19
Dektarion <andreiy.boyarsky@gmail.com>
2022-07-21 16:30:58 +0300

    feat:js task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
-}
+  if (matrix === undefined) {
+    return [];
+  }
+  
+  let spredArr = [];
+
+  if(matrix.length === 0){
+    return spredArr;
+  } else {
+  for (let i = 1; i < matrix.length; i = i + 2){
+    matrix[i].sort((a, b) => b - a);
+    console.log(matrix[i])
+    }
+  for (let i = 0; i < matrix.length; i++){
+    spredArr.push(...matrix[i]);
+    }
+  }
+  return spredArr;
+};
